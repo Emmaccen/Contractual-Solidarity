9b11d849125460bc1a8d6b6cfb16881888416eb1
emmaccen <oriolaemmanuel199@gmail.com>
2022-12-23 01:51:28 +0100

    On chain

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+// pragma solidity 0.8.8;
+ 
+contract SimpleStorage {  
+// Avalanche, Polygon, Fanthom
+    uint256 public favNumber;
+
+    function store (uint256 _favNumber) public{
+        favNumber = _favNumber;
+    }
+    mapping(string => People) public getPerson;
+
+    struct People {
+        uint256 age;
+        string name;
+    }
+    
+    // People public person = People({age : 23, name : "Emmanuel"});
+    People[] public person;
+
+
+    function createNewPerson(uint256 age, string memory name) public{
+        People memory newPerson = People(age, name);
+        person.push(newPerson);
+        getPerson[name] = newPerson;
+    }
+
+    function getFavNumber() public view returns(uint256){
+        return favNumber;
+    }
+}
