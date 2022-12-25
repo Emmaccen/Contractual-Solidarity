d0f428194d36eccd821e78977e54504e0b1d4714
emmaccen <oriolaemmanuel199@gmail.com>
2022-12-25 19:05:19 +0100

    Contract interactions "Storage Factory"

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >0.8.0;
+import "./SimpleStorage.sol";
+
+
+contract StorageFactory {
+    SimpleStorage public simpleStorage;
+
+    constructor(){
+        simpleStorage = new SimpleStorage();
+    }
+
+    function CreateNewSimpleStorageContact() public {
+           simpleStorage = new SimpleStorage();
+    }
+
+    function getFavFromSimpleStorage() public view returns(uint256){
+        return simpleStorage.getFavNumber();
+    } 
+    function callStoreFromSimpleStorage (uint256 _favNumber) public{
+       simpleStorage.store(_favNumber);
+    }
+}
